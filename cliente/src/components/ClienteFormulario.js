54989664a163cb9ebf416de1d9c96e8bb336b592
xavicolman <xavistcolman@gmail.com>
2022-06-20 20:29:47 -0400

    subiendo tabla

--- cliente/src/components/ClienteFormulario.js
+++ cliente/src/components/ClienteFormulario.js
@@ -1,31 +1,63 @@
-import { Grid, Typography, TextField, Button, Container, Box } from "@mui/material"
-import Link from '@mui/material/Link';
-
-
+import { Grid, Typography, TextField, Button, Box, CircularProgress } from "@mui/material";
+import Link from "@mui/material/Link";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="white" align="center" {...props}>
-      {'Copyright © '}
+    <Typography variant="body2" color="white" {...props}>
+      {"Copyright © "}
       <Link color="inherit" href="https://www.youtube.com/">
         Gabriel Colman
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 export default function ClienteFormulario() {
+  //Definimoa un estado para guardar las tareas
+  const [cliente, Setcliente] = useState({
+    nombre: "",
+    ci: "",
+    d_fecha_nacimiento: "",
+    direccion: "",
+    telefono: "",
+    obs: ""
+  });
 
-  const maneja_evento_submit = (evento) => {
+
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false);
+
+
+  const maneja_evento_submit = async (evento) => {
     evento.preventDefault();
-    console.log("Formulario enviado");
-  }
+    //console.log(cliente);
+
+    setLoading(true);
+    await fetch('http://localhost:1000/clientes', {
+      method: "POST",
+      body: JSON.stringify(cliente),
+      headers: { "Content-Type": "application/json" },
+    });
+    //const datos = await res.json();
+    //console.log(datos);
+    setLoading(false);
+    navigate('/')
+
+  };
+
+
+  const changeFormRegistro = (e) => {
+    //console.log(e.target.name, e.target.value);
+    Setcliente({ ...cliente, [e.target.name]: e.target.value.toUpperCase() });
+  };
+
 
   return (
-    <Container>
-
+    <Grid container alignItems="center" direction="column">
       <Box
         sx={{
           width: 650,
@@ -37,43 +69,60 @@ export default function ClienteFormulario() {
           alignItems: "center"
         }}
       >
-        <Typography component="h1" variant="h5" sx={{ mt: 2 }}  style={{ fontWeight: "bold" }}>
+        <Typography
+          component="h1"
+          variant="h5"
+          sx={{ mt: 2 }}
+          style={{ fontWeight: "bold", color: "white" }}
+        >
           Nuevo registro
         </Typography>
 
-        <Box component="form" sx={{ mt: 3, width: 500, height: 250 }}>
+        <Box
+          component="form"
+          onSubmit={maneja_evento_submit}
+          sx={{ mt: 3, width: 500, height: 250 }}
+        >
+
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
                 required
                 fullWidth
+                onChange={changeFormRegistro}
                 id="Nombre"
                 label="Nombre"
-                name="Nombre"
-                inputProps={{ style: { color: "white" } }}
+                name="nombre"
+                inputProps={{
+                  style: { color: "white", textTransform: "uppercase", disableUnderline: true }
+                }}
                 InputLabelProps={{ style: { color: "white" } }}
               />
             </Grid>
 
             <Grid item xs={12} sm={6}>
               <TextField
-                name="Cualquier"
+                name="ci"
                 required
                 fullWidth
-                id="firstName"
+                type="number"
+                id="ci"
                 label="Cédula"
+                onChange={changeFormRegistro}
                 autoFocus
                 inputProps={{ style: { color: "white" } }}
                 InputLabelProps={{ style: { color: "white" } }}
               />
             </Grid>
+
             <Grid item xs={12} sm={6}>
               <TextField
-                required
                 fullWidth
-                id="lastName"
+                id="telefono"
                 label="Telefono"
-                name="Telefono"
+                name="telefono"
+                type="number"
+                onChange={changeFormRegistro}
                 inputProps={{ style: { color: "white" } }}
                 InputLabelProps={{ style: { color: "white" } }}
               />
@@ -81,54 +130,67 @@ export default function ClienteFormulario() {
 
             <Grid item xs={12} sm={6}>
               <TextField
-                name="Cualquier"
+                name="d_fecha_nacimiento"
                 required
                 fullWidth
-                id="firstName"
+                id="d_fecha_nacimiento"
                 label="Feha de nacimiento"
                 autoFocus
                 type="date"
+                onChange={changeFormRegistro}
                 inputProps={{ style: { color: "white" } }}
+                InputLabelProps={{ style: { color: "white" }, shrink: true }}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="direccion"
+                label="Dirección"
+                name="direccion"
+                onChange={changeFormRegistro}
+                inputProps={{
+                  style: { color: "white", textTransform: "uppercase" }
+                }}
                 InputLabelProps={{ style: { color: "white" } }}
               />
             </Grid>
 
             <Grid item xs={12}>
               <TextField
-                required
                 fullWidth
-                id="Nombre"
-                label="Dirección"
-                name="Nombre"
-                inputProps={{ style: { color: "white" } }}
-                InputLabelProps={{ style: { color: "white" } }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="Nombre"
+                id="obs"
                 label="Observaciones"
-                name="Nombre"
+                name="obs"
+                onChange={changeFormRegistro}
                 multiline
                 rows={4}
-                inputProps={{ style: { color: "white" } }}
+                inputProps={{
+                  style: { color: "white", textTransform: "uppercase" }
+                }}
                 InputLabelProps={{ style: { color: "white" } }}
               />
+
+
               <Button
                 type="submit"
                 fullWidth
                 variant="contained"
                 sx={{ mt: 3 }}
               >
-                Guardar
+                {loading ? (
+                  <CircularProgress color="inherit" size={24} />
+                ) : (
+                  "Guardar"
+                )}
               </Button>
+
             </Grid>
             <Copyright sx={{ mt: 5 }} />
           </Grid>
         </Box>
       </Box>
-    </Container>
+    </Grid>
   );
 }
--- cliente/src/components/ClienteLista.js
+++ cliente/src/components/ClienteLista.js
@@ -1,7 +1,80 @@
 //el comando rfc es para crear componentes facilmente
+import { useEffect, useState } from "react"
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export default function ClienteLista() {
-  return (
-    <div>ClienteLista</div>
+
+  const [clientes, setClientes] = useState([]);
+
+    const busca_clientes = async () => {
+      const result = await fetch('http://localhost:1000/clientes');
+      const datos = await result.json();
+      //console.log(datos);
+      setClientes(datos);
+    }
+
+    useEffect(() => {
+      busca_clientes();
+    }, []);
+
+    const datos = [
+      { id: 1, lastName: 'Francisco colman', cedula: '5255054', fecha_nacimiento: '08/04/1997', direccion: 'Tavapy' , telefono: '0973635373', observacion: "Hola mundo dede una observacion" },
+      { id: 2, lastName: 'Francisco colman', cedula: '5255054', fecha_nacimiento: '08/04/1997', direccion: 'Tavapy' , telefono: '0973635373', observacion: "Hola mundo dede una observacion" },
+      { id: 3, lastName: 'Francisco colman', cedula: '5255054', fecha_nacimiento: '08/04/1997', direccion: 'Tavapy' , telefono: '0973635373', observacion: "Hola mundo dede una observacion" },
+
+    ];
+    
+    return (
+      <>  
+      <h1>Clientes</h1>
+
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 600 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Nombre</TableCell>
+            <TableCell align="right">Cedula</TableCell>
+            <TableCell align="right">Fecha nacimiento</TableCell>
+            <TableCell align="right">Direccion</TableCell>
+            <TableCell align="right">Telefono</TableCell>
+            <TableCell align="right">Observacion</TableCell>
+          </TableRow>
+        </TableHead>
+
+
+        <TableBody>
+          {datos.map((datos) => (
+            <TableRow
+              key={datos.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {datos.lastName}
+              </TableCell>
+              <TableCell align="right">{datos.cedula}</TableCell>
+              <TableCell align="right">{datos.fecha_nacimiento}</TableCell>
+              <TableCell align="right">{datos.direccion}</TableCell>
+              <TableCell align="right">{datos.telefono}</TableCell>
+              <TableCell align="right">{datos.observacion}</TableCell>
+
+            </TableRow>
+          ))}
+        </TableBody>
+
+        </Table>
+
+        </TableContainer>
+
+
+
+      </>
+
   )
 }
